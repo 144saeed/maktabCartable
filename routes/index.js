c22d2162405a2c733e0d4d3d89dd67a9ee58f607
MohammadReza Paykanro <paykanro.mohammadreza@hotmail.com>
2018-06-29 11:52:57 +0430

    Addressing Problem

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
+var fs=require('fs');
 var router = express.Router();
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.sendFile('login.html', { title: 'Express' });
 });
 router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+  res.send('dashboard', { title: 'Express' });
 });
 router.get('/layout', function(req, res, next) {
   res.render('layout');
