b0d3579f4f3436fabfbb6c7323d0cf3c8f84fe0d
MohammadReza Paykanro <paykanro.mohammadreza@hotmail.com>
2018-06-29 11:55:44 +0430

    Addressing Problem 2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
+//app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(passport.initialize());
 app.use(passport.session());
