1079c25351217d90a0acf94afc02e2045cb9d858
MohammadReza Paykanro <paykanro.mohammadreza@hotmail.com>
2018-07-05 12:52:12 +0430

    Login Solved

--- app/routes.js
+++ app/routes.js
@@ -51,10 +51,15 @@ module.exports = function (app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/userRolls',isLoggedIn,function (req, res) {
+        database.userPersonalDataByUserId(req.user, (err, results, fields) => {
+            res.send(results);           
+        })
+    });
     app.get('/userAccounts', isLoggedIn, function (req, res) {
         database.userPersonalDataByUserId(req.user, (err, results, fields) => {
-            res.sendFile(res.sendFile(path.join(__dirname, '../views/roll.html'),results));
-            //With this route u will directed to rolls page to choose a roll wich u have.
+            res.sendFile(path.join(__dirname, '../views/roll.html'));
+            //With this route u will directed to rolls page to choose a roll wich user has.
         })
     });
 
