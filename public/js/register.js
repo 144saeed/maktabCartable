ce85b0241b99536d46fe61d2fc077e8e09208384
Unknown <ilkhani_hossein@yahoo.com>
2018-07-13 12:39:50 +0430

    change for utf

--- public/js/register.js
+++ public/js/register.js
@@ -2,7 +2,7 @@
 
 function check() {
     var data = getCookie('currentUserEmail')
-   // console.log("data : " + data)
+    // console.log("data : " + data)
     var pass = $('#password').val()
     var repass = $('#repassword').val()
     if (pass == "" || repass == "") {
@@ -17,42 +17,50 @@ function check() {
         $('#checkFild').css({ 'display': 'none' })
         $('#noMatch').css({ 'display': 'none' })
 
-        $.post("/initPasswordByUser", { 'password': $('#password').val()  , 'email':data }, function (data) {
-            if (data == 0) {
-                $('#faild').css({ 'display': 'block' })
-            }
-            else {
-                $('#success').css({ 'display': 'block' })
-                $.get('/')
+        $.ajax({
+            url: "/initPasswordByUser",
+            type: "POST",
+            data: { 'password': $('#password').val(), 'email': data },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                if (data == 0) {
+                    $('#faild').css({ 'display': 'block' })
+                }
+                else {
+                    $('#success').css({ 'display': 'block' })
+                    $.get('/')
+                }
             }
         })
+
     }
 }
 
 function getCookie(name) {
     if (document.cookie) {
-    var allCookie = document.cookie;
-   // var allCookie = 'a=1;b=;c=3;d=4'
-    var cookie = []
-    var arrayOfCookie = allCookie.split(`;`);
-    for (var i = 0 ; i < arrayOfCookie.length ; i++) {
-        var miniArrayOfCookie = arrayOfCookie[i].split('=')
+        var allCookie = document.cookie;
+        // var allCookie = 'a=1;b=;c=3;d=4'
+        var cookie = []
+        var arrayOfCookie = allCookie.split(`;`);
+        for (var i = 0 ; i < arrayOfCookie.length ; i++) {
+            var miniArrayOfCookie = arrayOfCookie[i].split('=')
 
-        cookie.push(miniArrayOfCookie[0])
-        cookie.push(miniArrayOfCookie[1])
+            cookie.push(miniArrayOfCookie[0])
+            cookie.push(miniArrayOfCookie[1])
 
-    }
-    //console.log(cookie)
-   // console.log(cookie.length)
-    for (var i = 0 ; i < cookie.length ; i++) {
-        if (cookie[i] == name) {
-           // console.log(cookie[i + 1])
-            return cookie[i + 1]
+        }
+        //console.log(cookie)
+        // console.log(cookie.length)
+        for (var i = 0 ; i < cookie.length ; i++) {
+            if (cookie[i] == name) {
+                // console.log(cookie[i + 1])
+                return cookie[i + 1]
+            }
         }
     }
-    }
-      else {
-         console.log("کوکی یافت نشد")
+    else {
+        console.log("کوکی یافت نشد")
         return "کوکی یافت نشد"
     }
 }
